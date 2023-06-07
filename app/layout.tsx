42c57456f3937f7ba541430693ccfbdc457458ca
Elem <elem15@yandex.ru>
2023-06-07 22:21:46 +0300

    feat: add auth with google

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import Header from '@/components/Header'
 import './globals.css'
 import { Inter } from 'next/font/google'
 import Footer from '@/components/Footer'
+import { Provider } from '@/components/Provider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,11 +19,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header />
-        <main className='content'>
-          {children}
-        </main>
-        <Footer />
+        <Provider>
+          <Header />
+          <main className='content'>
+            {children}
+          </main>
+          <Footer />
+        </Provider>
       </body>
     </html>
   )
--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,7 +1,7 @@
 'use client'
 import Link from 'next/link';
 import { usePathname } from 'next/navigation'
-
+import { useSession, signIn, signOut } from 'next-auth/react'
 type NavLink = {
   label: string;
   href: string;
@@ -12,12 +12,18 @@ type Props = {
 
 export default function Navigation({ navLinks }: Props) {
   const pathname = usePathname()
+  const session = useSession()
+  console.log(session)
   return (
     <>
       {navLinks.map(link => (
         <Link key={link.label} href={link.href} className={pathname === link.href ? 'active' : ''}>{link.label}</Link >
       ))
       }
+      {session?.data && <Link href="/profile">Profile</Link>}
+      {session?.data ? (
+        <Link href='#' onClick={() => signOut({ callbackUrl: '/' })}>Log out</Link>
+      ) : (<Link href="api/auth/signin">Sign in</Link>)}
     </>
   )
 }
